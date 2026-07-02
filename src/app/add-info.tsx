0319fceb8176afc5474d0e6e93136515fd3ffabc
Heberth Deza <heberth2788@gmail.com>
2026-07-02 18:27:34 -0500

    add style to AddInfo component

--- src/app/add-info.tsx
+++ src/app/add-info.tsx
@@ -1,8 +1,17 @@
 import { Text, View } from "react-native";
 
+const styles = {
+    card: {
+        backgroundColor: "lightblue",
+        padding: 10,
+        margin: 10,
+        borderRadius: 5, 
+    }
+}
+
 export default function AddInfo() {
     return (
-        <View>
+        <View style = {[ styles.card, { borderLeftColor: "black", borderLeftWidth: 5 } ]}>
             <Text>Add Info</Text>
         </View>
     );
